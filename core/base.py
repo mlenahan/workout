7f0686101723c71240f7324890f5c0f1152b5b04
m_lenahan <michael_lenahan@hotmail.com>
2021-04-19 18:23:37 +0100

    initial commit of code

---
+++ core/base.py
@@ -0,0 +1,21 @@
+class Musclegroup:
+    CHEST = 'Chest'
+    BACK = 'Back'
+    SHOULDERS = 'Shoulders'
+    BICEPS = 'Biceps'
+    TRICEPS = 'Triceps'
+    QUADS = 'Quads'
+    HAMSTRINGS = 'Hamstrings'
+    CALVES = 'Calves'
+    CARDIO = 'Cardio'
+    CHOICES = [
+        (CHEST, 'Chest'),
+        (BACK, 'Back'),
+        (SHOULDERS, 'Shoulders'),
+        (BICEPS, 'Biceps'),
+        (TRICEPS, 'Triceps'),
+        (QUADS, 'Quads'),
+        (HAMSTRINGS, 'Hamstrings'),
+        (CALVES, 'Calves'),
+        (CARDIO, 'Cardio')
+    ]
---
+++ service/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('exercises/', views.ExerciseList.as_view()),
+    path('exercises/<int:pk>/', views.ExerciseDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
