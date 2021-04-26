f0c8273051b50886fa514d53908911010f675aef
m_lenahan <michael_lenahan@hotmail.com>
2021-04-26 16:21:25 +0100

    updating models

--- core/models.py
+++ core/models.py
@@ -11,3 +11,40 @@ class Exercise(models.Model):
 
     class Meta:
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Set(models.Model):
+    exercise = models.ForeignKey('core.Exercise', null=False, blank=False, on_delete=models.CASCADE)
+    reps = models.PositiveIntegerField(null=False, blank=False, default=0)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    notes = models.TextField(null=False, blank=True, max_length=1000)
+    number_of_sets = models.IntegerField(null=False, blank=False, default=3)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.exercise) + ' - ' + str(self.reps) + ' - ' + str(self.weight)
+
+
+class Workout(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    exercise = models.ManyToManyField('core.Exercise')
+    set = models.ManyToManyField('core.Set')
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+
+# Workout -> User, CreatedAt, FinishedAt,
+
+# Set -> Workout(many to one), Reps, Exercise(many to one), Weight, Notes,  CreatedAt, RestTime??
+
+# ExerciseTag -> e.g. Quads, hamstrings..., Name, (many to many(put on exercise model))
+
+# User ->
+
+# TODO - model all of the above
